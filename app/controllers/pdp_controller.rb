7ddcc8c787692717d60e1c5169d835351642bd43
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2014-05-06 22:53:46 +0200

    Use another file path on server

--- app/controllers/pdp_controller.rb
+++ app/controllers/pdp_controller.rb
@@ -1,8 +1,8 @@
 class PdpController < ApplicationController
   def index
     name = '/Users/sander/Dropbox/M1.1/PDP/PDP.md'
+    name = '/home/sander/www/pdp.md' unless File.exist? name
     source = File.read name
-    #markdown = Redcarpet::Markdown.new PDPFile
     renderer = Redcarpet::Render::HTML.new with_toc_data: true, hard_wrap: true
     markdown = Redcarpet::Markdown.new renderer, footnotes: true, strikethrough: true
     html = markdown.render source
