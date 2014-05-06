9772022eb0e47ad1b579e449c3be23ec7c9f82c0
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2014-05-06 23:22:05 +0200

    Set relative URL root

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Pdp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_controller.relative_url_root = '/pdp'
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
