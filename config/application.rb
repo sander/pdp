86a3bc4fe5515697708049dfd9d17a76aedf9bc0
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2014-05-06 23:17:49 +0200

    Add fonts to assets paths

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Pdp
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.compass.require 'susy'
 
     # Settings in config/environments/* take precedence over those specified here.
