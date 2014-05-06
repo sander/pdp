8f7124812b180e9402fd8ec9d8f517788ffea8d4
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2014-05-06 22:49:38 +0200

    Small improvements

--- app/controllers/pdp_controller.rb
+++ app/controllers/pdp_controller.rb
@@ -6,6 +6,8 @@ class PdpController < ApplicationController
     renderer = Redcarpet::Render::HTML.new with_toc_data: true, hard_wrap: true
     markdown = Redcarpet::Markdown.new renderer, footnotes: true, strikethrough: true
     html = markdown.render source
+    @date = Time.now.strftime '%B %e, %Y'
+    @version = 'f'
     @pdp = html.html_safe
   end
 end
